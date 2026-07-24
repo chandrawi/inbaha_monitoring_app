d73a0c29b9e01e6cac9bcea73434271bf4e8b390
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2026-07-25 01:54:05 +0700

    Create TitleMenu and Navbar for index page

---
+++ src/components/navigation/NavbarIndex.tsx
@@ -0,0 +1,32 @@
+import { Show, createSignal } from "solid-js";
+import TitleMenuIndex from "./TitleMenuIndex";
+import SearchMenu from "./SearchMenu";
+import SettingMenu from "./SettingMenu";
+import ProfileMenu from "./ProfileMenu";
+
+export default function NavbarIndex() {
+
+  let [searchExpand, setSearchExpand] = createSignal(false);
+
+  return (
+    <div class="fixed z-30 top-0 w-full h-14 flex flex-row bg-white dark:bg-slate-900 shadow-md-res shadow-slate-200 dark:shadow-slate-950 text-gray-800 dark:text-gray-200">
+      <div class="lg:min-w-50 lg:w-[16%] xl:min-w-55 w-auto h-full flex flex-row justify-start lg:justify-center px-2 xs:px-3 md:px-4">
+        <div class="h-full flex flex-row items-center">
+          <img src="/image/logo_inbaha.png" alt="" class="h-10 ml-1 hidden md:inline" />
+        </div>
+      </div>
+      <div class="grow h-full flex flex-row justify-between">
+        <div class="grow h-full flex flex-row justify-center md:justify-start">
+          <Show when={!searchExpand()}>
+            <TitleMenuIndex />
+          </Show>
+        </div>
+        <div class="h-full flex flex-row">
+          <SearchMenu searchExpand={searchExpand} setSearchExpand={setSearchExpand} />
+          <SettingMenu />
+          <ProfileMenu />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/navigation/TitleMenuIndex.tsx
@@ -0,0 +1,82 @@
+export default function TitleMenuIndex() {
+  return (
+    <>
+    <div class="dropdown h-full static sm:relative">
+      <div class="w-full h-full overflow-hidden wrap-break-word">
+        <button tabindex="0" class="group h-full pl-3 pr-2.5 flex flex-row items-center justify-center md:justify-start hover:text-sky-800 sm:border-x border-slate-200 dark:hover:text-sky-300 dark:border-slate-700">
+          <span class="icon-monitoring text-[1.5rem] text-gray-500 group-hover:text-sky-950 dark:group-hover:text-sky-100"></span>
+          <h1 class="h-full leading-14 ml-1 mr-0.5 xs:ml-2 xs:mr-1 font-medium text-lg">Monitoring</h1>
+          <span class="icon-arrow_fill_down text-[1rem] text-sky-700"></span>
+        </button>
+      </div>
+      <div class="w-full sm:w-auto flex flex-row absolute left-0 justify-center sm:justify-start wrap-break-word">
+        <div class="dropdown-content w-80 bg-white shadow-md-res shadow-slate-200 rounded-b-sm dark:bg-gray-800 dark:shadow-slate-950">
+          <a href="/#/" class="w-full px-2 py-1.5 flex flex-row items-center border-t border-slate-200 hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+            <span class="icon-monitoring text-[1.25rem]"></span>
+            <span class="ml-1.5">Monitoring System</span>
+          </a>
+          <div class="w-full px-2 py-1.5 sm:hidden flex flex-row items-center border-t border-slate-200 dark:border-slate-700">
+            <span class="icon-grid_view text-[1.25rem]"></span>
+            <span class="ml-1.5">Category</span>
+          </div>
+          <div class="w-full block sm:hidden text-sm">
+            <a href="/#/category" class="w-full pl-8 pr-2 py-1 flex flex-row items-center border-t border-slate-200 border-dotted hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+              <span class="icon-list_square text-[1rem]"></span>
+              <span class="ml-1.5">----</span>
+            </a>
+          </div>
+          <div class="w-full px-2 py-1.5 sm:hidden flex flex-row items-center border-t border-slate-200 dark:border-slate-700">
+            <span class="icon-globe text-[1.25rem]"></span>
+            <span class="ml-1.5">Region</span>
+          </div>
+          <div class="w-full block sm:hidden text-sm">
+            <a href="/#/region" class="w-full pl-8 pr-2 py-1 flex flex-row items-center border-t border-slate-200 border-dotted hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+              <span class="icon-list_square text-[1rem]"></span>
+              <span class="ml-1.5">----</span>
+            </a>
+          </div>
+          <a href="/#/" class="w-full px-2 py-1.5 flex flex-row items-center border-t border-slate-200 hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+            <span class="icon-home text-[1.25rem]"></span>
+            <span class="ml-1.5">Home</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="dropdown h-full static sm:relative">
+      <div class="w-full h-full overflow-hidden wrap-break-word">
+        <button tabindex="0" class="group h-full pl-3 pr-2.5 hidden sm:flex flex-row items-center hover:text-sky-800 border-r border-slate-200 dark:hover:text-sky-300 dark:border-slate-700">
+          <span class="icon-grid_view text-[1.5rem] text-gray-500 group-hover:text-sky-950 dark:group-hover:text-sky-100"></span>
+          <h1 class="h-full leading-14 ml-1 mr-0.5 xs:ml-2 xs:mr-1 font-medium text-lg">Category</h1>
+          <span class="icon-arrow_fill_down text-[1rem] text-sky-700"></span>
+        </button>
+      </div>
+      <div class="w-full sm:w-auto flex flex-row wrap-break-word">
+        <div class="dropdown-content w-60 bg-white shadow-md-res shadow-slate-200 rounded-b-sm dark:bg-gray-800 dark:shadow-slate-950">
+          <a href="/#/category" class="w-full px-2 py-1.5 flex flex-row items-center border-t border-slate-200 hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+            <span class="icon-list_square text-[1rem]"></span>
+            <span class="ml-1.5">----</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="dropdown h-full static sm:relative">
+      <div class="w-full h-full overflow-hidden wrap-break-word">
+        <button tabindex="0" class="group h-full pl-3 pr-2.5 hidden sm:flex flex-row items-center hover:text-sky-800 border-r border-slate-200 dark:hover:text-sky-300 dark:border-slate-700">
+          <span class="icon-globe text-[1.5rem] text-gray-500 group-hover:text-sky-950 dark:group-hover:text-sky-100"></span>
+          <h1 class="h-full leading-14 ml-1 mr-0.5 xs:ml-2 xs:mr-1 font-medium text-lg">Region</h1>
+          <span class="icon-arrow_fill_down text-[1rem] text-sky-700"></span>
+        </button>
+      </div>
+      <div class="w-full sm:w-auto flex flex-row justify-end lg:justify-start wrap-break-word">
+        <div class="dropdown-content w-60 bg-white shadow-md-res shadow-slate-200 rounded-b-sm dark:bg-gray-800 dark:shadow-slate-950">
+          <a href="/#/region" class="w-full px-2 py-1.5 flex flex-row items-center border-t border-slate-200 hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+            <span class="icon-list_square text-[1rem]"></span>
+            <span class="ml-1.5">----</span>
+          </a>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+  

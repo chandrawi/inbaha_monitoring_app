5c8f53b779e8504cf01713bc9466bda064b6201b
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2026-07-25 02:06:09 +0700

    Create TitleMenu and Navbar for dashboard page

--- src/components/navigation/NavbarDashboard.tsx
+++ src/components/navigation/NavbarDashboard.tsx
@@ -1,25 +1,30 @@
 import { Show, createSignal } from "solid-js";
-import TitleMenuIndex from "./TitleMenuIndex";
+import TitleMenuDashboard from "./TitleMenuDashboard";
 import SearchMenu from "./SearchMenu";
 import SettingMenu from "./SettingMenu";
 import ProfileMenu from "./ProfileMenu";
 
-export default function NavbarIndex() {
+export default function NavbarDashboard() {
 
   let [searchExpand, setSearchExpand] = createSignal(false);
 
   return (
     <div class="fixed z-30 top-0 w-full h-14 flex flex-row bg-white dark:bg-slate-900 shadow-md-res shadow-slate-200 dark:shadow-slate-950 text-gray-800 dark:text-gray-200">
-      <div class="lg:min-w-50 lg:w-[16%] xl:min-w-55 w-auto h-full flex flex-row justify-start lg:justify-center px-2 xs:px-3 md:px-4">
+      <div class="md:min-w-62.5 md:w-[20%] w-auto h-full flex flex-row justify-center px-2 xs:px-3 md:px-4">
+        <button class="md:hidden h-full flex items-center">
+          <label for="sidebar" class="drawer-button cursor-pointer h-5">
+            <span class="icon-menu_list text-[1.25rem]"></span>
+          </label>
+        </button>
         <div class="h-full flex flex-row items-center">
-          <img src="/image/logo_inbaha_min.png" alt="" class="h-9 ml-1 inline md:hidden" />
+          <img src="/image/logo_inbaha_min.png" alt="" class="h-8 w-8 ml-1 inline md:hidden" />
           <img src="/image/logo_inbaha.png" alt="" class="h-10 ml-1 hidden md:inline" />
         </div>
       </div>
       <div class="grow h-full flex flex-row justify-between">
         <div class="grow h-full flex flex-row justify-center md:justify-start">
           <Show when={!searchExpand()}>
-            <TitleMenuIndex />
+            <TitleMenuDashboard />
           </Show>
         </div>
         <div class="h-full flex flex-row">
---
+++ src/components/navigation/TitleMenuDashboard.tsx
@@ -0,0 +1,32 @@
+export default function TitleMenuDashboard() {
+  return (
+    <div class="dropdown h-full static sm:relative">
+      <div class="w-full h-full flex flex-row justify-center md:justify-start overflow-hidden break-all">
+        <button class="group h-full flex flex-row items-center justify-center md:justify-start hover:text-sky-800 dark:hover:text-sky-300 overflow-hidden break-all">
+          <span class="icon-dashboard text-[1.5rem] text-gray-500 group-hover:text-sky-950 dark:group-hover:text-sky-100"></span>
+          <h1 class="h-full leading-14 ml-1 mr-0.5 xs:ml-2 xs:mr-1 font-medium text-lg sm:hidden">Dashboard</h1>
+          <h1 class="h-full leading-14 ml-1 mr-0.5 xs:ml-2 xs:mr-1 font-medium text-lg hidden sm:inline">Dashboard Monitor</h1>
+          <span class="icon-arrow_fill_down text-[1rem] text-sky-700"></span>
+        </button>
+      </div>
+      <div class="w-full sm:w-auto flex flex-row absolute left-0 justify-center sm:justify-start wrap-break-word">
+        <div class="dropdown-content w-80 bg-white shadow-md-res shadow-slate-200 rounded-b-sm dark:bg-gray-800 dark:shadow-slate-950">
+          <a class="w-full px-2 py-1.5 flex flex-row items-center border-t border-slate-200 hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300"
+            href="/"
+          >
+            <span class="icon-monitoring text-[1rem]"></span>
+            <span class="ml-1.5">Monitoring System</span>
+          </a>
+          <a class="w-full px-2 py-1.5 flex flex-row items-center border-t border-slate-200 hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+            <span class="icon-grid_view text-[1rem]"></span>
+            <span class="ml-1.5">Category: </span>
+          </a>
+          <a class="w-full px-2 py-1.5 flex flex-row items-center border-t border-slate-200 hover:text-sky-800 dark:border-slate-700 dark:hover:text-sky-300">
+            <span class="icon-globe text-[1rem]"></span>
+            <span class="ml-1.5">Region: </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}

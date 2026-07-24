db3656ccfaa4c4fc372d0a1ab26bb72b06901aab
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2026-07-24 23:50:10 +0700

    Add some fonts, icons, and images

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider, Title, Link } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -10,6 +10,8 @@ export default function App() {
       root={props => (
         <MetaProvider>
           <Title>SolidStart</Title>
+          <Link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;600;700;900&amp;display=swap" />
+          <Link rel="stylesheet" href="/fonts/bbthings_icon.css" />
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}

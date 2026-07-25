edfb58a16070cfc70aba4d925d180a4ba9db1149
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2026-07-25 22:11:14 +0700

    Modify index and not found page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,13 +1,29 @@
 import { Title } from "@solidjs/meta";
 import { HttpStatusCode } from "@solidjs/start";
+import NavbarIndex from "~/components/navigation/NavbarIndex";
+import { darkTheme } from "~/lib/store";
 
 export default function NotFound() {
   return (
     <>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <div class="text-center p-4">
-        <h1 class="text-4xl uppercase my-4">Page Not Found</h1>
+      <div classList={{ dark: darkTheme() }} class="h-screen overflow-hidden">
+        <NavbarIndex />
+        <div class="min-h-[calc(100vh-3.5rem)] mt-14 bg-slate-50 dark:bg-slate-800 text-gray-800 dark:text-gray-200 flex items-center justify-center">
+          <div class="bg-white w-[40%] min-w-72 mb-14 rounded-sm shadow-md shadow-slate-300 dark:bg-gray-900 dark:shadow-slate-950">
+            <div class="flex px-3 py-2 bg-gray-100 dark:bg-gray-800 border-x border-t border-slate-200 dark:border-slate-800">
+              <span class="icon-cross text-[1.75rem] mr-2"></span>
+              <span class="text-xl font-medium">Page Not Found</span>
+            </div>
+            <div class="flex justify-center py-5 text-xl">
+              <a href="/" class="py-1.5 px-3 bg-sky-700 hover:bg-sky-800 rounded-sm">
+                <span class="icon-home text-gray-100 hover:text-white mr-2"></span>
+                <span class="text-gray-100 hover:text-white">Home</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
 import { Title } from "@solidjs/meta";
+import { RouteSectionProps } from "@solidjs/router";
+import NavbarIndex from "~/components/navigation/NavbarIndex";
+import { darkTheme } from "~/lib/store";
 
-export default function Home() {
+export default function Home(props: RouteSectionProps) {
   return (
     <>
       <Title>Home</Title>
+      <div classList={{ dark: darkTheme() }} class="h-screen overflow-hidden">
+        <NavbarIndex />
+        <div class="min-h-[calc(100vh-3.5rem)] mt-14 bg-slate-50 dark:bg-slate-800 text-gray-800 dark:text-gray-200 overflow-auto scrollbar-custom">
+          {props.children}
+        </div>
+      </div>
     </>
   );
 }
